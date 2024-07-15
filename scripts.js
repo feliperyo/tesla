af68ad75d0e38b29684627d90e5847905a76fc75
feliperyo <ryofeliperyo@gmail.com>
2024-07-15 13:35:15 -0300

    finalizando projeto

---
+++ scripts.js
@@ -0,0 +1,18 @@
+let formDrive = document.querySelector(".formulario-drive");
+let formBuy = document.querySelector(".formulario-buy");
+
+function showFormDrive() {
+  formDrive.style.right = "50%";
+}
+
+function hideFormDrive() {
+  formDrive.style.right = "-50%";
+}
+
+function showFormBuy() {
+  formBuy.style.left = "50%";
+}
+
+function hideFormBuy() {
+  formBuy.style.left = "-50%";
+}
